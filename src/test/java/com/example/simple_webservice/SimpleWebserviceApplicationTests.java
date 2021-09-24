7f58313ae658a37421a8549bf6c4db3559750528
daehee-Hone <rhakeogml00@naver.com>
2021-09-24 19:53:32 +0900

    Initial commit

---
+++ src/test/java/com/example/simple_webservice/SimpleWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
